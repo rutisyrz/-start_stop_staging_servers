0a993d45cf7064b81d430ca7ed65b7c38a21a51e
Rutvij Pandya <rutisyrz@gmail.com>
2016-08-31 13:44:59 +0530

    IncorrectInstanceState exception handled

--- lib/aws_helper/ec2_instance.rb
+++ lib/aws_helper/ec2_instance.rb
@@ -10,49 +10,61 @@ class AwsHelper::EC2Instance
 	end
 
 	def stop(instance_id)
-		instance = @ec2.instance(instance_id)
-		# validate instance
-		validation_result = validate_instance(instance)
-		return validation_result if validation_result[:error].present?		
-		# get current state of instance
-		state = instance.state			
-	  case state.code
-	  when 0  # pending
-	  	return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_pending", instance_id: instance_id)}, result: nil}		    
-	  when 32  # shutting-down
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_shutting_down", instance_id: instance_id)}, result: nil}
-	  when 48  # terminated
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_terminated", instance_id: instance_id)}, result: nil}
-	  when 64  # stopping
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_stopping", instance_id: instance_id)}, result: nil}
-	  when 80  # stopped
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_stopped", instance_id: instance_id)}, result: nil}
-	  else
-	    result = instance.stop
-	    return {error: {}, result: result}
-	  end
+		begin
+			instance = @ec2.instance(instance_id)
+			# validate instance
+			validation_result = validate_instance(instance)
+			return validation_result if validation_result[:error].present?		
+			# get current state of instance
+			state = instance.state			
+		  case state.code
+		  when 0  # pending
+		  	return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_pending", instance_id: instance_id)}}		    
+		  when 32  # shutting-down
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_shutting_down", instance_id: instance_id)}}
+		  when 48  # terminated
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_terminated", instance_id: instance_id)}}
+		  when 64  # stopping
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_stopping", instance_id: instance_id)}}
+		  when 80  # stopped
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_stopped", instance_id: instance_id)}}
+		  else
+		    result = instance.stop
+		    return {error: {}, result: result}
+		  end
+		rescue Aws::EC2::Errors::IncorrectInstanceState => e
+			return {error: {code: I18n.t("aws.common.codes.incorrect_instance_state"), message: e.message}}
+	  rescue Exception => e
+			return {error: {code: I18n.t("aws.common.codes.custom_exception"), message: I18n.t("aws.common.errors.custom_exception")}}
+		end
 	end
 
 	def start(instance_id)
-		instance = @ec2.instance(instance_id)
-		# validate instance
-		validation_result = validate_instance(instance)
-		return validation_result if validation_result[:error].present?		
-		# get current state of instance
-		state = instance.state
-	  case state.code
-	  when 0  # pending
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_pending", instance_id: instance_id)}, result: nil}
-	  when 32  # shutting-down
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_shutting_down", instance_id: instance_id)}, result: nil}
-	  when 16  # started
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_started", instance_id: instance_id)}, result: nil}
-	  when 48  # terminated
-	    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_terminated", instance_id: instance_id)}, result: nil}
-	  else
-	    result = instance.start
-	    return {error: {}, result: result}
-	  end		
+		begin
+			instance = @ec2.instance(instance_id)
+			# validate instance
+			validation_result = validate_instance(instance)
+			return validation_result if validation_result[:error].present?		
+			# get current state of instance
+			state = instance.state
+		  case state.code
+		  when 0  # pending
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_pending", instance_id: instance_id)}}
+		  when 32  # shutting-down
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_shutting_down", instance_id: instance_id)}}
+		  when 16  # started
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_started", instance_id: instance_id)}}
+		  when 48  # terminated
+		    return {error: {code: state.name, message: I18n.t("aws.ec2.errors.instance_terminated", instance_id: instance_id)}}
+		  else
+		    result = instance.start
+		    return {error: {}, result: result}
+		  end		
+		rescue Aws::EC2::Errors::IncorrectInstanceState => e
+			return {error: {code: I18n.t("aws.common.codes.incorrect_instance_state"), message: e.message}, result: []}
+	  rescue Exception => e
+			return {error: {code: I18n.t("aws.common.codes.custom_exception"), message: I18n.t("aws.common.errors.custom_exception")}}
+		end
 	end	
 	
 	def validate_instance(instance)
@@ -62,9 +74,9 @@ class AwsHelper::EC2Instance
 			end
 			return {error: {} }
 		rescue Aws::EC2::Errors::InvalidInstanceIDMalformed => e
-			return {error: {code: I18n.t("aws.common.codes.invalid_instance"), message: e.message}, result: []}
+			return {error: {code: I18n.t("aws.common.codes.invalid_instance"), message: e.message}}
 		rescue Exception => e
-			return {error: {code: I18n.t("aws.common.codes.custom_exception"), message: I18n.t("aws.common.errors.custom_exception")}, result: []}
+			return {error: {code: I18n.t("aws.common.codes.custom_exception"), message: I18n.t("aws.common.errors.custom_exception")}}
 		end
 	end
 end
